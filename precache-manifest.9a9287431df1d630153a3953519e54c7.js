aaa1fc1732b072eb5d691176b8fd0f2e5e27dc21
huunhan93 <nhan111321@gmail.com>
2019-10-17 08:48:00 +0700

    abc

---
+++ precache-manifest.9a9287431df1d630153a3953519e54c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c31090e6ff5839242838b0cd7f4a28ef",
+    "url": "/reactjs-redux/index.html"
+  },
+  {
+    "revision": "4943509507e8f011ed53",
+    "url": "/reactjs-redux/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "26f8c12a48a9a913d239",
+    "url": "/reactjs-redux/static/js/2.223134ec.chunk.js"
+  },
+  {
+    "revision": "4943509507e8f011ed53",
+    "url": "/reactjs-redux/static/js/main.d64d2299.chunk.js"
+  },
+  {
+    "revision": "8a3da417e367337244f0",
+    "url": "/reactjs-redux/static/js/runtime-main.7fe9a9d1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/reactjs-redux/static/media/logo.25bf045c.svg"
+  }
+]);
